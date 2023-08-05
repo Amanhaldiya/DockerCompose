cb83959956f2074dde1e6b313ce3935f197b6e99
Aman Haldiya <Amanhaldiya512@gmail.com>
2023-08-05 19:00:40 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Dockerizing React app for staging,testing but not for Production.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Not for Production 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
